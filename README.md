1baa70ea0d8e2ded6bc26c80bdbe25a9808f9363
loganrehberg <loganrehberg7@gmail.com>
2023-08-07 22:46:34 -0400

    added all INDEX.JS elements

---
+++ README.md
@@ -0,0 +1,34 @@
+
+  # poop simulator
+  
+  ## Description
+  undefined
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  undefined
+  
+  ## Usage
+  undefined
+  
+  ## License
+  undefined
+  undefined
+  
+  ## Contributing
+  undefined
+  
+  ## Tests
+  undefined
+  
+  ## Questions
+  GitHub: [undefined](https://github.com/undefined)
+  Email: undefined
+      
--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'Enter the title of your project',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md file generated successfully!');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+      const userResponses = await inquirer.prompt(questions);
+  
+      // Generate the README content using userResponses
+  
+      const readmeContent = `
+  # ${userResponses.projectTitle}
+  
+  ## Description
+  ${userResponses.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${userResponses.installation}
+  
+  ## Usage
+  ${userResponses.usage}
+  
+  ## License
+  ${userResponses.licenseBadge}
+  ${userResponses.licenseNotice}
+  
+  ## Contributing
+  ${userResponses.contributing}
+  
+  ## Tests
+  ${userResponses.tests}
+  
+  ## Questions
+  GitHub: [${userResponses.githubUsername}](https://github.com/${userResponses.githubUsername})
+  Email: ${userResponses.email}
+      `;
+  
+      writeToFile('README.md', readmeContent);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  // Function call to initialize app
+  init();
 
 // Function call to initialize app
 init();
