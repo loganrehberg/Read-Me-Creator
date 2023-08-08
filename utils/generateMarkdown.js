f011387ea072590d09f2baf6f7310c4a9de3fac7
loganrehberg <loganrehberg7@gmail.com>
2023-08-07 23:03:15 -0400

    generateMarkdown.js (completed)

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,71 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  }
+  // Add more conditions for other licenses if needed
+  return '';
+}
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT License](https://opensource.org/licenses/MIT)';
+  }
+  // Add more conditions for other licenses if needed
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+This project is licensed under the ${renderLicenseLink(license)}.`;
+  }
+  return '';
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+
   return `# ${data.title}
 
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(data.license) ? '- [License](#license)' : ''}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername})
+Email: ${data.email}
 `;
 }
 
 module.exports = generateMarkdown;
+
